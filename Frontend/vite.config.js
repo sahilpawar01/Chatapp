1fd2793554b1d50b23da2c59dee6bb299b8787f4
harpan <146012578+sahilpawar01@users.noreply.github.com>
2025-01-26 12:05:25 +0530

    Revert .gitignore after deployment

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 4001,
     proxy: {
       "/api": {
-        target: "http://localhost:5002",
+        target: "https://chatapp-ktcw.onrender.com",
         changeOrigin: true,
       },
     },
